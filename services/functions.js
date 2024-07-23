2498f64ce01c8936416ddec81f985e33fe429cd1
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-07-23 23:17:24 +0530

    ADD: Read user functionality

--- services/functions.js
+++ services/functions.js
@@ -12,3 +12,9 @@ createUser('Abhishek Kumar Gupta','Dumka',22,7992417318,'Jharkhand');  //User 1
 createUser('John Doe','Kormangala',32,9892417318,'Karnataka');  // User 2
 createUser('Ram Sharma','Patna',30,7777417318,'Bihar');  // User 2
 
+//READ USER FUNCTION
+function readUsers(){
+    return userData;
+}
+const allUsers=readUsers();
+console.log(allUsers);
