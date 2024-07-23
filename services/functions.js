03327796abd66a9ffc5032170fb1a58ea3b6fac6
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-07-23 23:36:46 +0530

    ADD: update user functionality

--- services/functions.js
+++ services/functions.js
@@ -1,20 +1,39 @@
-import {User} from "../model/user.js";
+import { User } from "../model/user.js";
 
-const userData=[]; 
+const userData = [];
 
 //CREATE USER FUNCTION
-function createUser(fullName,address,age,number,state){
-    const user = new User(fullName,address,age,number,state);
+function createUser(fullName, address, age, number, state) {
+    const user = new User(fullName, address, age, number, state);
     userData.push(user);
 }
 
-createUser('Abhishek Kumar Gupta','Dumka',22,7992417318,'Jharkhand');  //User 1
-createUser('John Doe','Kormangala',32,9892417318,'Karnataka');  // User 2
-createUser('Ram Sharma','Patna',30,7777417318,'Bihar');  // User 2
+createUser('Abhishek Kumar Gupta', 'Dumka', 22, 7992417318, 'Jharkhand');  //User 1
+createUser('John Doe', 'Kormangala', 32, 9892417318, 'Karnataka');  // User 2
+createUser('Ram Sharma', 'Patna', 30, 7777417318, 'Bihar');  // User 2
 
 //READ USER FUNCTION
-function readUsers(){
+function readUsers() {
     return userData;
 }
-const allUsers=readUsers();
+const allUsers = readUsers();
+console.log(allUsers);
+
+
+//UPDATE USER FUNCTION
+function findUserByName(name) {
+    return userData.find(user => user.fullName == name);
+}
+
+function updateUser(name, newDetails) {
+    const user = findUserByName(name);
+    if (user) {
+        Object.assign(user, newDetails);
+        return true;
+    }
+    return false;
+}
+
+const updated = updateUser('Ram Sharma', { address: 'Bhagalpur', age: 25 });
+console.log(updated);
 console.log(allUsers);
