f67df2aff49d1b63178a02680f621a54dc112fad
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-07-23 22:12:01 +0530

    ADD: user schema

---
+++ model/user.js
@@ -0,0 +1,11 @@
+export class User{
+    constructor(fullName,address,age,number,state){
+        this.fullName=fullName;
+        this.address=address;
+        this.age=age;
+        this.number=number;
+        this.state=state;
+    }
+}
+
+
